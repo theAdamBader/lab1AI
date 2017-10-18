86013ef2f56d6066984541f3ed7d0911bf1615ab
Adam Bader <abade011@gold.ac.uk>
2017-10-18 11:11:23 +0100

    better default

--- lab1_tanks/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ lab1_tanks/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -22,7 +22,7 @@ namespace Complete
                     return TrackBehaviour();
 
                 default:
-                    return new Root (new Action(()=> Turn(0.1f)));
+				return new Root (DefaultMove());
             }
         }
 
@@ -36,6 +36,28 @@ namespace Complete
             return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));
         }
 
+		private Node DefaultMove(){
+		return new Root(
+				new Service(0.2f, UpdatePerception,
+					new Selector(
+						new BlackboardCondition("targetOffCentre",
+							Operator.IS_SMALLER_OR_EQUAL, 0.1f,
+							Stops.IMMEDIATE_RESTART,
+							// Stop turning and fire
+							new Sequence(StopTurning(),
+								new Wait(2f),
+								RandomFire())),
+						new BlackboardCondition("targetOnRight",
+							Operator.IS_EQUAL, true,
+							Stops.IMMEDIATE_RESTART,
+							// Turn right toward target
+							new Action(() => Turn(0.2f))),
+						// Turn left toward target
+						new Action(() => Turn(-0.2f))
+					)
+				)
+			);
+		}
 
         /* Example behaviour trees */
 
