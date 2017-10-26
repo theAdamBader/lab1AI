7af817127dd8ad1de36be59945c65e32ba6a8786
Adam Bader <abade011@gold.ac.uk>
2017-10-26 23:43:44 +0100

    75% commenting, only 25% left to comment then...I shall be...

--- lab1_tanks/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
+++ lab1_tanks/Assets/_Completed-Assets/Scripts/Tank/Behaviours.cs
@@ -1,3 +1,12 @@
+/*
+These are 3 cases of my own behaviours of the AI and a default which was teaked from Jeremy Gow's TrackBehaviour AI
+
+REFERENCE:
+Jeremy Gow's Tanks! Behaviour Tree AI - modified scripts File (Track Behaviour code): https://learn.gold.ac.uk/course/view.php?id=6843&section=2
+NP Behave Unity List: http://unitylist.com/r/69f/np-behave
+
+*/
+
 using UnityEngine;
 using NPBehave;
 using System.Collections.Generic;
@@ -24,36 +33,29 @@ namespace Complete
 				return YouAreUnbelievable();
 
 			default:
-				return new Root (DefaultMove());
+				return new Root (SuchFun());//this Root calls the default case, SuchFun
 			}
 		}
 
 		/* Actions */
 		private Node  StopMoving(){
-			return new Action(() => Move(0));
+			return new Action(() => Move(0)); //This would stop the movement of the AI
 		}
 
 		private Node StopTurning() {
-			return new Action(() => Turn(0));
-		}
-
-		private Node NoFire() {
-			return new Action(() => Fire(0));
+			return new Action(() => Turn(0));//This would stop the turning of the AI
 		}
 
 		private Node RandomFire() {
-			return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));
+			return new Action(() => Fire(UnityEngine.Random.Range(0.0f, 1.0f)));//This would randomly fire at the target
 		}
 
-		private Node RandomTurn() {
-			return new Action(() => Turn(UnityEngine.Random.Range(1.0f, -1.0f)));
-		}
-
-		private Node RandomMove() {
-			return new Action(() => Move(UnityEngine.Random.Range(0.0f, -0.8f)));
-		}
-
-		private Node DefaultMove(){
+		//SuchFun is case 0 (Fun)
+		//this is a private node that contains the selectors for the default case
+		//it has an interval of 0.2 and calls UpdatePerception 
+		//the first blackboard uses the of centre which once it tracks the target it stops turns and randomly fires
+		//the second blackboard checks the x axis of the plane to track where the tank is heading to, if right then turns; else it turns left
+		private Node SuchFun(){
 			return new Service(0.2f, UpdatePerception,
 				new Selector(
 					new BlackboardCondition("targetOffCentre",
@@ -73,7 +75,12 @@ namespace Complete
 			);
 		}
 
-		//DONE
+		//FightMe is case 1 (Deadly)
+		//first backboard does NOT see the target at front as the operator is false so when the player is behind then the AI stops moving and turns
+		//second blackboard uses the of centre which once it tracks the target it stops turns and randomly fires; however, unlike the default case it does NOT wait for 0.5f, it waits 0.1f before randomly firing
+		//third backboard is a random behaviour which would run every time the selector changes, probablity at 5%, and as it is set at a low priority, it would stop once the condition is met and the root proceeds to its next node
+		//forth blackboard checks that if the target is near and when near it slows down compared to the third blackboard
+		//fifth blackboard checks if the target is on the right then move right else move left
 		private Root FightMe()
 		{
 			return new Root(
@@ -81,50 +88,50 @@ namespace Complete
 					new Selector(
 
 						new BlackboardCondition("targetInFront",
-							Operator.IS_EQUAL, false,
+							Operator.IS_EQUAL, false,//as it's false, it detects the target from behind
 							Stops.IMMEDIATE_RESTART,
-							// Turn right toward target
-							new Sequence(StopMoving(),
+							new Sequence(StopMoving(),//calls the node, StopMoving
 								new Wait(0.5f),
-								new Action(() => Turn(1.0f)))),
+								new Action(() => Turn(1.0f)))),//turns to the right as fast as possible
 			
 						new BlackboardCondition("targetOffCentre",
 							Operator.IS_SMALLER_OR_EQUAL, 0.1f,
 							Stops.IMMEDIATE_RESTART,
-							// Stop turning and fire
 							new Sequence(StopTurning(),
 								new Wait(0.1f),
-								RandomFire())),
-						
-						new NPBehave.Random(0.05f,new BlackboardCondition("targetDistance",//5%
-							Operator.IS_SMALLER_OR_EQUAL, 50.0f,//if player is 40 pixels near the enemy then enemy moves
+								RandomFire())),//fires after 0.1f
+
+						new NPBehave.Random(0.05f,new BlackboardCondition("targetDistance",//it would randomly, at 5%, go through this node
+							Operator.IS_SMALLER_OR_EQUAL, 50.0f,//if target is 50 pixels near the enemy then enemy moves
 							Stops.LOWER_PRIORITY,
 
 							new Sequence(new Action(() => Move(1.0f)),
 								new Wait(0.7f),
 								RandomFire()))),
-						
-						new BlackboardCondition("targetDistance",
-							Operator.IS_SMALLER_OR_EQUAL, 10.0f,
-							Stops.IMMEDIATE_RESTART,
 
+						new BlackboardCondition("targetDistance",
+							Operator.IS_SMALLER_OR_EQUAL, 10.0f,//if target is 10 pixels near the enemy then enemy moves
+							Stops.IMMEDIATE_RESTART,
+							//moves forward towards the enemy
 							new Action(() => Move(0.6f))),
 						
-
 						new BlackboardCondition("targetOnRight",
 							Operator.IS_EQUAL, true,
 							Stops.IMMEDIATE_RESTART,
-							// Turn right toward target
+							// turns right toward target
 							new Action(() => Turn(0.6f))),
-						// Turn left toward target
+						// turns left toward target
 						new Action(() => Turn(-0.6f))
-
-
 					)
 				)
 			);
 		}
 
+		//ScaredyCat is case 2 (Frightened)
+		//first blackboard detects if there are anything, target or objects, in front of it and would reverse and turn slowly 
+		//second blackboard tracks if the target is near, if it is it moves faster than the third blackboard
+		//third blackboard tracks if the target is quite near however, it is random that this node would pass through and using low priority, it would stop once the condition is met 
+		//forth blackboard checks if the target is on the right then it would move to the left to try and avoid the target; else it moves right when target is on the left
 		private Root ScaredyCat()
 		{
 			return new Root(
@@ -134,7 +141,7 @@ namespace Complete
 						new BlackboardCondition("targetInFront",
 							Operator.IS_EQUAL, true,
 							Stops.IMMEDIATE_RESTART,
-							// Turn right toward target
+					
 							new Sequence(new Action(() => Move(-0.4f)),
 								new Wait(1.2f),
 								new Action(() => Turn(0.2f)))),
@@ -143,22 +150,20 @@ namespace Complete
 							Operator.IS_SMALLER_OR_EQUAL, 10.0f,
 							Stops.IMMEDIATE_RESTART,
 
-							new Action(() => Move(0.8f))),
+							new Action(() => Move(1.5f))),
 
 						new NPBehave.Random(0.15f,new BlackboardCondition("targetDistance",//5%
-							Operator.IS_SMALLER_OR_EQUAL, 20.0f,//if player is 40 pixels near the enemy then enemy moves
+							Operator.IS_SMALLER_OR_EQUAL, 20.0f,//if player is 20 pixels near the enemy then enemy moves
 							Stops.LOWER_PRIORITY,
 
 							new Action(() => Move(1.0f)))),
 								
-						
-								
 						new BlackboardCondition("targetOnRight",
-							Operator.IS_EQUAL, true,//if the targetOnRight is true then turn right towards the target
+							Operator.IS_EQUAL, true,//if the targetOnRight is true then turn left towards the target
 							Stops.IMMEDIATE_RESTART,
 							new Action(() => Turn(-0.2f))),
 
-						new Action(() => Turn(0.2f))//else it will turn left towards the target
+						new Action(() => Turn(0.2f))//else it will right left towards the target
 					)
 				)
 			);
